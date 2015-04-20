ad54ab240cd455b211524e8cb7f670882e83fd48
maciek <maku17@gmail.com>
2015-04-20 22:04:40 +0200

    adding command part

--- server/src/main/java/pl/net/nowak/task/api/rest/TaskRestApi.java
+++ server/src/main/java/pl/net/nowak/task/api/rest/TaskRestApi.java
@@ -2,6 +2,8 @@ package pl.net.nowak.task.api.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import pl.net.nowak.core.CommandBus;
+import pl.net.nowak.task.api.FinishTaskCommand;
 import pl.net.nowak.task.api.TaskAPI;
 import pl.net.nowak.task.api.dto.TaskDTO;
 
@@ -18,6 +20,8 @@ public class TaskRestApi {
     @Autowired
     TaskAPI taskAPI;
 
+    @Autowired
+    CommandBus bus;
 
     @RequestMapping(method = RequestMethod.GET)
     public @ResponseBody List<TaskDTO> getTasks() {
@@ -26,9 +30,13 @@ public class TaskRestApi {
 
     @RequestMapping(method = RequestMethod.POST)
     public void addTask(@RequestBody TaskDTO dto) {
-
         taskAPI.addNew(dto);
+    }
 
+    //TODO verb in url !!!
+    @RequestMapping(value = "/{id}/finish" , method = RequestMethod.PUT)
+    public void finishTask(@PathVariable String id) {
+        bus.handle(new FinishTaskCommand(id));
     }
 
 }
